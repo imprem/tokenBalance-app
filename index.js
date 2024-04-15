43afddc9aa938e84f949933730c751ae9d03072c
premchand <premch995@gmail.com>
2024-04-15 18:54:41 +0530

    added code

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-console.log('Hello from tokenBalance-app!');
+const axios = require('axios');
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3008;
+
+async function fetchData(url) {
+    try {
+        const response = await axios.get(url);
+        return response.data.response.currentBalance;
+    } catch (error) {
+        throw new Error('Failed to fetch data');
+    }
+}
+
+async function fetchtier1() {
+    return await fetchData('http://13.56.68.120:10090/dashboard/info/default/primarySupplier?nodeName=Tier1Supplier');
+}
+
+async function fetchtier2() {
+    return await fetchData('http://13.56.68.120:10090/dashboard/info/secondarySupplier/emailId?nodeName=Tier2Supplier&emailId=admin@subconOne.com');
+}
+
+app.get('/tier1', async (req, res) => {
+    try {
+        const data = await fetchtier1();
+        res.send(data);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
+app.get('/tier2', async (req, res) => {
+    try {
+        const data = await fetchtier2();
+        res.send(data);
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
